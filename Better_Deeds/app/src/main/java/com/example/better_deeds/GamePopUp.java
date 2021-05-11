23e69b401f6be42849d0a3582e85caf52bdf1911
Maria Ailah Rolda <rolda.mariaailah@ue.edu.ph>
2021-05-11 20:08:35 +0800

    New Activity

---
+++ Better_Deeds/app/src/main/java/com/example/better_deeds/GamePopUp.java
@@ -0,0 +1,13 @@
+package com.example.better_deeds;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamePopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_pop_up);
+    }
+}
